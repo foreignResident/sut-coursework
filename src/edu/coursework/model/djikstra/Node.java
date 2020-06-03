73073177b50fd458cdd7d84248fb179254929e3b
Annchouss <anyashi2002@gmail.com>
2020-06-03 17:21:22 +0300

    Add node

---
+++ src/edu/coursework/model/djikstra/Node.java
@@ -0,0 +1,26 @@
+package edu.coursework.model.djikstra;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Node {
+
+    private String name;
+
+    private List<Node> shortestPath = new LinkedList<>();
+
+    private Integer distance = Integer.MAX_VALUE;
+
+    Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+    public void addDestination(Node destination, int distance) {
+        adjacentNodes.put(destination, distance);
+    }
+
+    public Node(String name) {
+        this.name = name;
+    }
+
+}
